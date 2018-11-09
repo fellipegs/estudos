301a1019ab01725596c7670fc0e2a7b165f377ab
Fellipe <fellipe.santos@bluesoft.com.br>
2018-11-09 16:54:29 -0200

    praticas das aulas

--- lojadecamisetas/app/controllers/produtos_controller.rb
+++ lojadecamisetas/app/controllers/produtos_controller.rb
@@ -1,2 +1,7 @@
 class ProdutosController < ApplicationController
+
+	def index
+		@produtos = Produto.order :nome
+		@produtos = Produto.order :preco
+	end
 end
--- lojadecamisetas/config/routes.rb
+++ lojadecamisetas/config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
  get "produtos" => "produtos#index"
+
+ root :to => "produtos#index"
+
 end
