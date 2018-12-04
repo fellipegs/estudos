88cf7b57a9e731e70a4032e866a98c3ba85cdca5
Fellipe <fellipe.santos@bluesoft.com.br>
2018-12-04 19:28:37 -0200

    adicionando buscas

--- lojadecamisetas/app/controllers/produtos_controller.rb
+++ lojadecamisetas/app/controllers/produtos_controller.rb
@@ -4,11 +4,21 @@ class ProdutosController < ApplicationController
 		@produtos = Produto.order :nome
 		@produtos = Produto.order :preco
 	end
+	
+	def busca
+		@nome_a_buscar = params[:nome]
+		@produtos = Produto.where "nome like ?", "%#{@nome_a_buscar}%"
+	end
+
 
 	def create
 		valores = params.require(:produto).permit :nome, :preco, :descricao, :quantidade
 		produto = Produto.create valores
-		redirect_to new_produto_path
+		if produto.save
+				redirect_to new_produto_path
+		else
+			render :new
+		end		
 	end
 
 	def destroy
--- lojadecamisetas/app/models/application_record.rb
+++ lojadecamisetas/app/models/application_record.rb
@@ -1,3 +1,4 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+  validates :quantidade, presence: true
 end
